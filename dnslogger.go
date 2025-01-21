748d5644c3ffd6d28c7d36635d95adf3b9aa32b0
Jonathas Ribeiro <jonathas@starti.com.br>
2025-01-21 12:38:14 -0300

    test 7

--- dnslogger.go
+++ dnslogger.go
@@ -5,7 +5,6 @@ import (
 	"github.com/coredns/coredns/plugin"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
 	"github.com/coredns/coredns/plugin/pkg/replacer"
-	"github.com/coredns/coredns/plugin/pkg/response"
 	"github.com/coredns/coredns/request"
 
 	"github.com/miekg/dns"
@@ -14,23 +13,15 @@ import (
 // DNSLogger is a basic request logging plugin.
 type DNSLogger struct {
 	Next plugin.Handler
-
 	repl replacer.Replacer
-}
-
-// ServeDNS implements the plugin.Handler interface.
-func (dl DNSLogger) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	state := request.Request{W: w, Req: r}
-	clog.Info(state.IP(), state.Name())
-	return plugin.NextOrFailure(dl.Name(), dl.Next, ctx, w, r)
 }
 
 // Name implements the Handler interface.
 func (dl DNSLogger) Name() string { return "dnslogger" }
 
-// Rule configures the logging plugin.
-type Rule struct {
-	NameScope string
-	Class     map[response.Class]struct{}
-	Format    string
+// ServeDNS implements the plugin.Handler interface.
+func (dl DNSLogger) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	state := request.Request{W: w, Req: r}
+	clog.Info(state.IP(), state.Name())
+	return plugin.NextOrFailure(state.Name(), dl.Next, ctx, w, r)
 }
